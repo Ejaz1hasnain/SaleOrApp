47285bf53b4a45b1e66e406f3aec688c38ad926c
Ejaz1hasnain <ejazh@mindfiresolutions.com>
2024-10-28 15:53:53 +0530

    props type changed

--- app/(dashboard)/products/[id]/page.tsx
+++ app/(dashboard)/products/[id]/page.tsx
@@ -1,12 +1,12 @@
-'use client'
+'use client';
 import React from "react";
 import { ProductDetails } from "./ProductDetails";
 import { Product, useGetProductDetailsQuery } from "@/generated/graphql";
 import { use } from 'react';
 
-const ProductPage = (props: { params: React.Usable<{ id: string; }>; }) => {
-  const params = use(props?.params);
-  const decodedId = decodeURIComponent(params.id);
+const ProductPage = ({ params }: { params: Promise<{ id: string }> }) => {
+  const resolvedParams = use(params);
+  const decodedId = decodeURIComponent(resolvedParams.id);
 
   const { data, error, loading } = useGetProductDetailsQuery({
     variables: { id: decodedId }
@@ -15,9 +15,11 @@ const ProductPage = (props: { params: React.Usable<{ id: string; }>; }) => {
   if (loading) return <div className="text-center justify-center">Loading...</div>;
   if (error) return <div className="text-center justify-center">Error: {error.message}</div>;
 
-  return data?.product && (
+  return data?.product ? (
     <ProductDetails productDetails={data.product as Product} />
-  )
+  ) : (
+    <div className="text-center justify-center">Product not found</div>
+  );
 };
 
 export default ProductPage;
